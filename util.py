464ab3b4132153733b12c2c0136751ad3a993ffa
horace <hp264@cornell.edu>
2016-07-06 19:00:08 -0400

    added some util functions

--- util.py
+++ util.py
@@ -14,3 +14,21 @@ def gen_categorical(category, df, max_categories=10, quantile=False):
         percentiles = np.arange(0, 1, 1.0/max_categories)
         buckets = np.percentile(df[category], percentiles)
     return df[category].apply(lambda x: bisect.bisect_left(buckets, x))
+
+def normalize_column(category, df, inplace=False):
+    mean = np.nanmean(df[category])
+    col_max = np.max(df[category])
+    col_min = np.min(df[category])
+    normed = df[category].apply(lambda x: x if pd.isnan(x) else (x - mean)/(col_max-col_min), axis=1)
+
+    if inplace:
+        df[category] = normed
+    else:
+        return normed 
+
+def sq_loss(true_y, predicted_y):
+    return np.sum(np.square(true_y - predicted_y))
+
+def clean_percent(val):
+    return float(val[:-1])
+
