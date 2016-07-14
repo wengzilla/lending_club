db90b0191e63aa2fa6c364881a7a3ffe13e9f4a1
horace <hp264@cornell.edu>
2016-07-14 14:20:53 -0400

    fixed label issue messing up the confusion mtrix

--- group_models.py
+++ group_models.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
@@ -24,8 +25,9 @@ def fit(X, y, group_feat, model_func, params):
     models = {}
 
     for gname, X_group in X.groupby(group_feat):
+        print "Making group for group %s" %gname
         X_group = X_group.drop(group_feat, axis=1)
-        y_group = y.loc[X_group.index]
+        y_group = y.ix[X_group.index]
         model = model_func(**params)
         model.fit(X_group, y_group)
         models[gname] = model
@@ -60,3 +62,31 @@ def predicted_indices(X, group_feat, group_models):
         print("Selected %d things from model on group %s" %(len(good_indices), gname))
 
     return indices
+
+def group_conf_matrices(X, y, group_feat, group_models):
+    pass
+
+def group_results(X, y, payouts, group_feat, group_models):
+    grouped = X.groupby(group_feat)
+    all_indices = []
+    true_pos = 0
+    false_pos = 0
+ 
+    for gname, group in grouped:
+        X_group = group.drop('grade', axis=1)
+        y_group = y.ix[X_group.index]
+
+        predicted = group_models[gname].predict(X_group)
+        cm = confusion_matrix(y_group, predicted, labels=[True, False])
+        precision = cm[0,0] / np.sum(cm[:, 0])
+        indices = [X_group.index[i] for i in range(len(X_group)) if predicted[i] == 1]
+        all_indices.extend(indices)
+        #if cm.shape[0] >= 2:
+        true_pos += cm[0,0]
+        false_pos += cm[1,0]
+        print "Precision for group %s: %.2f" %(gname, precision)
+        print "Benchmark for group %s: %.2f" %(gname, np.mean(payouts.ix[indices]))
+        print cm
+
+    print "Overall precision: %d %d" %(true_pos, false_pos)
+    print "Overall benchmark: %.2f" %np.mean(payouts.ix[all_indices])
